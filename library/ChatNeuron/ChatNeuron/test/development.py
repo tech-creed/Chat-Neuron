c46d637baa1743f6fee05f86c024bed3afb84106
MUKILAN S <mukilan069@gmail.com>
2023-03-04 15:41:15 +0530

    Chat Neuron Library Model Training

--- library/ChatNeuron/ChatNeuron/test/development.py
+++
@@ -1,8 +0,0 @@
-import chatNeuron as cn
-
-questions, answers  = cn.dataPreparation('../csvTest.csv')
-vocab_len, tokenizer = cn.gstTokenizer(questions, answers)
-encoderInput, decoderInput, decoderOutput = cn.Tokenize(questions, answers, tokenizer, vocab_len)
-
-print(encoderInput, decoderInput, decoderOutput)
-
